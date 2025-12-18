cc75c22c8cfc8c8b3995fa0593f1e62c02e06a70
Jérôme Benoit <jerome.benoit@piment-noir.org>
2025-12-18 21:38:17 +0100

    chore(qav3): bump versions
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- quickadapter/user_data/freqaimodels/QuickAdapterRegressorV3.py
+++ quickadapter/user_data/freqaimodels/QuickAdapterRegressorV3.py
@@ -72,7 +72,7 @@ class QuickAdapterRegressorV3(BaseRegressionModel):
     https://github.com/sponsors/robcaulk
     """
 
-    version = "3.7.135"
+    version = "3.7.136"
 
     _TEST_SIZE: Final[float] = 0.1
 
